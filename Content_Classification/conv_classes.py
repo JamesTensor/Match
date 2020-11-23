93786ed5b8a856cd64513b0e5e9d485ca3163b6d
王嘉杰 <wangjiajie@boohee.com>
2020-11-23 15:50:52 +0800

    转移错了，吐血

---
+++ Content_Classification/conv_classes.py
@@ -0,0 +1,303 @@
+import numpy
+import theano.tensor.shared_randomstreams
+import theano
+import theano.tensor as T
+from sklearn.linear_model.logistic import LogisticRegression
+from theano.tensor.signal import downsample
+from theano.tensor.nnet import conv
+
+def ReLU(x):
+    y = T.maximum(0.0, x)
+    return(y)
+def Sigmoid(x):
+    y = T.nnet.sigmoid(x)
+    return(y)
+def Tanh(x):
+    y = T.tanh(x)
+    return(y)
+def Iden(x):
+    y = x
+    return(y)
+
+class HiddenLayer(object):
+    """
+    Class for HiddenLayer
+    """
+    def __init__(self, rng, input, n_in, n_out, activation, W=None, b=None,
+                 use_bias=False):
+
+        self.input = input
+        self.activation = activation
+
+        if W is None:
+            if activation.func_name == "ReLU":
+                W_values = numpy.asarray(0.01 * rng.standard_normal(size=(n_in, n_out)), dtype=theano.config.floatX)
+            else:
+                W_values = numpy.asarray(rng.uniform(low=-numpy.sqrt(6. / (n_in + n_out)), high=numpy.sqrt(6. / (n_in + n_out)),
+                                                     size=(n_in, n_out)), dtype=theano.config.floatX)
+            W = theano.shared(value=W_values, name='W')
+        if b is None:
+            b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
+            b = theano.shared(value=b_values, name='b')
+
+        self.W = W
+        self.b = b
+
+        if use_bias:
+            lin_output = T.dot(input, self.W) + self.b
+        else:
+            lin_output = T.dot(input, self.W)
+
+        self.output = (lin_output if activation is None else activation(lin_output))
+
+        # 模型参数
+        if use_bias:
+            self.params = [self.W, self.b]
+        else:
+            self.params = [self.W]
+
+def _dropout_from_layer(rng, layer, p):
+    """p is the probablity of dropping a unit
+"""
+    srng = theano.tensor.shared_randomstreams.RandomStreams(rng.randint(999999))
+    # p=1-p because 1's indicate keep and p is prob of dropping
+    mask = srng.binomial(n=1, p=1-p, size=layer.shape)
+    # The cast is important because
+    # int * float32 = float64 which pulls things off the gpu
+    output = layer * T.cast(mask, theano.config.floatX)
+    return output
+
+class DropoutHiddenLayer(HiddenLayer):
+    def __init__(self, rng, input, n_in, n_out,
+                 activation, dropout_rate, use_bias, W=None, b=None):
+        super(DropoutHiddenLayer, self).__init__(
+                rng=rng, input=input, n_in=n_in, n_out=n_out, W=W, b=b,
+                activation=activation, use_bias=use_bias)
+
+        self.output = _dropout_from_layer(rng, self.output, p=dropout_rate)
+
+class MLPDropout(object):
+    """A multilayer perceptron with dropout"""
+    def __init__(self,rng,input,layer_sizes,dropout_rates,activations,use_bias=True):
+
+        #rectified_linear_activation = lambda x: T.maximum(0.0, x)
+
+        # Set up all the hidden layers
+        self.weight_matrix_sizes = zip(layer_sizes, layer_sizes[1:])
+        self.layers = []
+        self.dropout_layers = []
+        self.activations = activations
+        next_layer_input = input
+        #first_layer = True
+        # dropout the input
+        next_dropout_layer_input = _dropout_from_layer(rng, input, p=dropout_rates[0])
+        layer_counter = 0
+        for n_in, n_out in self.weight_matrix_sizes[:-1]:
+            next_dropout_layer = DropoutHiddenLayer(rng=rng,
+                    input=next_dropout_layer_input,
+                    activation=activations[layer_counter],
+                    n_in=n_in, n_out=n_out, use_bias=use_bias,
+                    dropout_rate=dropout_rates[layer_counter])
+            self.dropout_layers.append(next_dropout_layer)
+            next_dropout_layer_input = next_dropout_layer.output
+
+            # Reuse the parameters from the dropout layer here, in a different
+            # path through the graph.
+            next_layer = HiddenLayer(rng=rng,
+                    input=next_layer_input,
+                    activation=activations[layer_counter],
+                    # scale the weight matrix W with (1-p)
+                    W=next_dropout_layer.W * (1 - dropout_rates[layer_counter]),
+                    b=next_dropout_layer.b,
+                    n_in=n_in, n_out=n_out,
+                    use_bias=use_bias)
+            self.layers.append(next_layer)
+            next_layer_input = next_layer.output
+            #first_layer = False
+            layer_counter += 1
+
+        # Set up the output layer
+        n_in, n_out = self.weight_matrix_sizes[-1]
+        dropout_output_layer = LogisticRegression(
+                input=next_dropout_layer_input,
+                n_in=n_in, n_out=n_out)
+        self.dropout_layers.append(dropout_output_layer)
+
+        # Again, reuse paramters in the dropout output.
+        output_layer = LogisticRegression(
+            input=next_layer_input,
+            # scale the weight matrix W with (1-p)
+            W=dropout_output_layer.W * (1 - dropout_rates[-1]),
+            b=dropout_output_layer.b,
+            n_in=n_in, n_out=n_out)
+        self.layers.append(output_layer)
+
+        # Use the negative log likelihood of the logistic regression layer as
+        # the objective.
+        self.dropout_negative_log_likelihood = self.dropout_layers[-1].negative_log_likelihood
+        self.dropout_errors = self.dropout_layers[-1].errors
+
+        self.negative_log_likelihood = self.layers[-1].negative_log_likelihood
+        self.errors = self.layers[-1].errors
+
+        # Grab all the parameters together.
+        self.params = [ param for layer in self.dropout_layers for param in layer.params ]
+
+    def predict(self, new_data):
+        next_layer_input = new_data
+        for i, layer in enumerate(self.layers):
+            if i < len(self.layers) - 1:
+                next_layer_input = self.activations[i](T.dot(next_layer_input, layer.W) + layer.b)
+            else:
+                p_y_given_x = T.nnet.softmax(T.dot(next_layer_input, layer.W) + layer.b)
+        y_pred = T.argmax(p_y_given_x, axis=1)
+        return y_pred
+
+    def predict_p(self, new_data):
+        next_layer_input = new_data
+        for i, layer in enumerate(self.layers):
+            if i < len(self.layers) - 1:
+                next_layer_input = self.activations[i](T.dot(next_layer_input, layer.W) + layer.b)
+            else:
+                p_y_given_x = T.nnet.softmax(T.dot(next_layer_input, layer.W) + layer.b)
+        return p_y_given_x
+
+class MLP(object):
+    """Multi-Layer Perceptron Class
+
+    """
+
+    def __init__(self, rng, input, n_in, n_hidden, n_out):
+
+        self.hiddenLayer = HiddenLayer(rng=rng, input=input,
+                                       n_in=n_in, n_out=n_hidden,
+                                       activation=T.tanh)
+
+        # The logistic regression layer gets as input the hidden units
+        # of the hidden layer
+        self.logRegressionLayer = LogisticRegression(
+            input=self.hiddenLayer.output,
+            n_in=n_hidden,
+            n_out=n_out)
+
+
+        self.negative_log_likelihood = self.logRegressionLayer.negative_log_likelihood
+        self.errors = self.logRegressionLayer.errors
+
+        self.params = self.hiddenLayer.params + self.logRegressionLayer.params
+
+
+class LogisticRegression(object):
+
+
+    def __init__(self, input, n_in, n_out, W=None, b=None):
+        """ Initialize the parameters of the logistic regression
+    :type input: theano.tensor.TensorType
+    :param input: symbolic variable that describes the input of the
+    architecture (one minibatch)
+
+    :type n_in: int
+    :param n_in: number of input units, the dimension of the space in
+    which the datapoints lie
+
+    :type n_out: int
+    :param n_out: number of output units, the dimension of the space in
+    which the labels lie
+
+    """
+
+        if W is None:
+            self.W = theano.shared(
+                value=numpy.zeros((n_in, n_out), dtype=theano.config.floatX),
+                name='W')
+        else:
+            self.W = W
+
+        if b is None:
+            self.b = theano.shared(
+                value=numpy.zeros((n_out,), dtype=theano.config.floatX),
+                name='b')
+        else:
+            self.b = b
+
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+
+
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+
+        self.params = [self.W, self.b]
+
+    def negative_log_likelihood(self, y):
+
+        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+
+    def errors(self, y):
+
+
+        if y.ndim != self.y_pred.ndim:
+            raise TypeError('y should have the same shape as self.y_pred',
+                            ('y', target.type, 'y_pred', self.y_pred.type))
+        if y.dtype.startswith('int'):
+            return T.mean(T.neq(self.y_pred, y))
+        else:
+            raise NotImplementedError()
+
+class LeNetConvPoolLayer(object):
+    """ """
+
+    def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2), non_linear="tanh"):
+
+        assert image_shape[1] == filter_shape[1]
+        self.input = input
+        self.filter_shape = filter_shape
+        self.image_shape = image_shape
+        self.poolsize = poolsize
+        self.non_linear = non_linear
+        # there are "num input feature maps * filter height * filter width"
+        # inputs to each hidden unit
+        fan_in = numpy.prod(filter_shape[1:])
+        # each unit in the lower layer receives a gradient from:
+        # "num output feature maps * filter height * filter width" /
+        #   pooling size
+        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) /numpy.prod(poolsize))
+        # initialize weights with random weights
+        if self.non_linear=="none" or self.non_linear=="relu":
+            self.W = theano.shared(numpy.asarray(rng.uniform(low=-0.01,high=0.01,size=filter_shape),
+                                                dtype=theano.config.floatX),borrow=True,name="W_conv")
+        else:
+            W_bound = numpy.sqrt(6. / (fan_in + fan_out))
+            self.W = theano.shared(numpy.asarray(rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
+                dtype=theano.config.floatX),borrow=True,name="W_conv")
+        b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
+        self.b = theano.shared(value=b_values, borrow=True, name="b_conv")
+
+        # convolve input feature maps with filters
+        conv_out = conv.conv2d(input=input, filters=self.W,filter_shape=self.filter_shape, image_shape=self.image_shape)
+        if self.non_linear=="tanh":
+            conv_out_tanh = T.tanh(conv_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+            self.output = downsample.max_pool_2d(input=conv_out_tanh, ds=self.poolsize, ignore_border=True)
+        elif self.non_linear=="relu":
+            conv_out_tanh = ReLU(conv_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+            self.output = downsample.max_pool_2d(input=conv_out_tanh, ds=self.poolsize, ignore_border=True)
+        else:
+            pooled_out = downsample.max_pool_2d(input=conv_out, ds=self.poolsize, ignore_border=True)
+            self.output = pooled_out + self.b.dimshuffle('x', 0, 'x', 'x')
+        self.params = [self.W, self.b]
+
+    def predict(self, new_data, batch_size):
+        """
+        predict for new data
+        """
+        img_shape = (batch_size, 1, self.image_shape[2], self.image_shape[3])
+        conv_out = conv.conv2d(input=new_data, filters=self.W, filter_shape=self.filter_shape, image_shape=img_shape)
+
+        if self.non_linear == "tanh":
+            conv_out_tanh = T.tanh(conv_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+            output = downsample.max_pool_2d(input=conv_out_tanh, ds=self.poolsize, ignore_border=True)
+        if self.non_linear == "relu":
+            conv_out_tanh = ReLU(conv_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+            output = downsample.max_pool_2d(input=conv_out_tanh, ds=self.poolsize, ignore_border=True)
+        else:
+            pooled_out = downsample.max_pool_2d(input=conv_out, ds=self.poolsize, ignore_border=True)
+            output = pooled_out + self.b.dimshuffle('x', 0, 'x', 'x')
+        return output
